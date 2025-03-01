c71236d8753feb78a1337863c88e5b4b1c4605f1
SajalRawat <sajalrawat2007@gmail.com>
2025-03-01 16:36:59 +0530

    sr

---
+++ 01_Basics/06_numsandmaths.js
@@ -0,0 +1,26 @@
+let score = 100
+let number = new Number(100)
+console.log(score,number);
+
+console.log(score.toString());
+// converting it into string to do more methods on it
+
+
+let balance = 234.567
+console.log(balance.toFixed(2));
+// this converts and precises value to a number after decimals
+
+
+console.log(balance.toPrecision(3));
+// retuns string and is a different function
+
+
+const hundreds = 10000000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+
+// **************************************************MATHS************************************************
